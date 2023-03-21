09a2faa93f3c7825d2dbeb278e101cbfedf575f8
zemun2007 <73990901+zemun2007@users.noreply.github.com>
2023-03-21 10:29:08 +0100

    Add files via upload

---
+++ HTML LOGIN/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["4x Kickbox World Champion", 
+    "Multi Milioner", "Top G"],
+
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
